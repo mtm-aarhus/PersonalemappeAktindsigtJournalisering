53a0a425cf2b96f4bf4f8f068b12c01612ddf937
Laura-Salmonsen <balas@aarhus.dk>
2025-10-29 14:48:15 +0100

    new queue name

--- robot_framework/config.py
+++ robot_framework/config.py
@@ -19,7 +19,7 @@ ERROR_EMAIL = "balas"
 # ----------------------
 
 # The name of the job queue (if any)
-QUEUE_NAME = 'PersonalemappeAktindsigtNySagEmail'
+QUEUE_NAME = 'PersonalemappeAktindsigtJournalisering'
 
 # The limit on how many queue elements to process
 MAX_TASK_COUNT = 100
